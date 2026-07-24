5fe35fd800639bbe5055309b11022f24f934d20c
antux18 <antux18@gmail.com>
2026-07-24 12:39:42 +0200

    feat: Talos LinkAliases generation and injection in kernel command line arguments for PXE boot.
    Signed-off-by: antux18 <antux18@gmail.com>

--- internal/superphenix-operator/cluster/talos_manager.go
+++ internal/superphenix-operator/cluster/talos_manager.go
@@ -1,9 +1,12 @@
 package cluster
 
 import (
+	"bytes"
 	"context"
+	"encoding/base64"
 	"fmt"
 	"maps"
+	"strings"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -11,6 +14,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/klauspost/compress/zstd"
 	operatorv1alpha1 "github.com/super-phenix/superphenix/api/operator/v1alpha1"
 	"github.com/super-phenix/superphenix/internal/superphenix-operator/version"
 )
@@ -44,6 +48,16 @@ func (r *Reconciler) reconcileTalosManager(ctx context.Context, cluster *operato
 			maps.Copy(values, talosManagerConfig)
 		} else {
 			return err
+		}
+
+		// If we use PXE boot, we need to tag network interfaces
+		// with a deterministic name (using Talos LinkAliases),
+		// because it will be used for the initial network configuration
+		// injected via kernel command line arguments by talos-manager:
+		if cluster.Spec.TalosManagementMode == operatorv1alpha1.TalosManagementFull {
+			if err := r.injectLinkAliases(ctx, &values); err != nil {
+				return err
+			}
 		}
 
 		app := r.initTalosManagerApp(fmt.Sprintf("%s-%s", TalosManagerApp, cluster.Name))
@@ -128,20 +142,94 @@ func (r *Reconciler) buildTalosManagerAppSpec(vals map[string]any) map[string]an
 				"prune":    true,
 				"selfHeal": true,
 			},
-			"syncOptions": []any{
-				"RespectIgnoreDifferences=true",
-			},
-		},
-		// The "kernelCmdlineArgs" field is modified by the job "linkaliases-setup" from the talos-manager chart after deployment, so it should be ignored by ArgoCD:
-		"ignoreDifferences": []any{
-			map[string]any{
-				"group": "talos.alperen.cloud",
-				"kind":  "TalosCluster",
-				"jqPathExpressions": []any{
-					".spec.controlPlane.metalSpec.machines[].pxeClientSpec.kernelCmdlineArgs",
-					".spec.worker.metalSpec.machines[].pxeClientSpec.kernelCmdlineArgs",
-				},
-			},
 		},
 	}
+}
+
+// injectLinkAliases generates the Talos LinkAliases for all the nodes and injects them in the kernel command line arguments
+func (r *Reconciler) injectLinkAliases(ctx context.Context, vals *map[string]any) error {
+	nodes, ok := (*vals)["nodes"].([]any)
+	if !ok {
+		return fmt.Errorf("Incorrect type for 'nodes' in 'talosManagerConfiguration' ('%T' instead of '[]any')", (*vals)["nodes"])
+	}
+	for _, item := range nodes {
+		node, ok := item.(map[string]any)
+		if !ok {
+			return fmt.Errorf("Incorrect type for node in 'nodes' array ('%T' instead of 'map[string]any')", item)
+		}
+		// Generate LinkAliases:
+		linkAliases := ""
+		interfaceSpec, ok := node["interface"].(map[string]any)
+		if !ok {
+			return fmt.Errorf("Incorrect type for 'interface' in node ('%T' instead of 'map[string]any')", node["interface"])
+		}
+		lacpBond, ok := interfaceSpec["lacpBond"].(map[string]any)
+		if !ok {
+			return fmt.Errorf("Incorrect type for 'interface.lacpBond' in node ('%T' instead of 'map[string]any')", interfaceSpec["lacpBond"])
+		}
+		lacpEnabled, ok := lacpBond["enabled"].(bool)
+		if !ok {
+			return fmt.Errorf("Incorrect type for 'interface.lacpBond.enabled' in node ('%T' instead of 'bool')", lacpBond["enabled"])
+		}
+		if lacpEnabled {
+			interfaces, ok := lacpBond["physicalInterfaces"].([]any)
+			if !ok {
+				return fmt.Errorf("Incorrect type for 'interface.lacpBond.physicalInterfaces' in node ('%T' instead of '[]any')", lacpBond["physicalInterfaces"])
+			}
+			for _, item := range interfaces {
+				iface, ok := item.(map[string]any)
+				if !ok {
+					return fmt.Errorf("Incorrect type for interface in 'interface.lacpBond.physicalInterfaces' array in node ('%T' instead of 'map[string]any')", item)
+				}
+				name, ok := iface["name"].(string)
+				if !ok {
+					return fmt.Errorf("Incorrect type for 'interface.lacpBond.physicalInterfaces[].name' in node ('%T' instead of 'string')", iface["name"])
+				}
+				macAddress, ok := iface["macAddress"].(string)
+				if !ok {
+					return fmt.Errorf("Incorrect type for 'interface.lacpBond.physicalInterfaces[].macAddress' in node ('%T' instead of 'string')", iface["macAddress"])
+				}
+				linkAliases = fmt.Sprintf("%s\n---\napiVersion: v1alpha1\nkind: LinkAliasConfig\nname: %s\nselector:\n  match: mac(link.permanent_addr) == \"%s\"", linkAliases, name, strings.ToLower(macAddress))
+			}
+		} else {
+			name, ok := interfaceSpec["name"].(string)
+			if !ok {
+				return fmt.Errorf("Incorrect type for 'interface.name' in node ('%T' instead of 'string')", interfaceSpec["name"])
+			}
+			macAddress, ok := interfaceSpec["physicalMacAddress"].(string)
+			if !ok {
+				return fmt.Errorf("Incorrect type for 'interface.physicalMacAddress' in node ('%T' instead of 'string')", interfaceSpec["physicalMacAddress"])
+			}
+			linkAliases = fmt.Sprintf("apiVersion: v1alpha1\nkind: LinkAliasConfig\nname: %s\nselector:\n  match: mac(link.permanent_addr) == \"%s\"", name, strings.ToLower(macAddress))
+		}
+
+		// Prepare LinkAliases for injection using the 'talos.config.inline' option.
+		// This option takes a Talos configuration string compressed with zstd and encoded in base64.
+		// zstd compression:
+		var linkAliasesZstd bytes.Buffer
+		writer, err := zstd.NewWriter(&linkAliasesZstd)
+		if err != nil {
+			return err
+		}
+		_, err = writer.Write([]byte(linkAliases))
+		if err != nil {
+			return err
+		}
+		if err = writer.Close(); err != nil {
+			return err
+		}
+		// base64 encoding:
+		linkAliasesFinal := fmt.Sprintf("talos.config.inline=%s", base64.StdEncoding.EncodeToString(linkAliasesZstd.Bytes()))
+
+		// Inject into kernel command line arguments:
+		_, ok = node["kernelCmdlineArgs"].(string)
+		if ok {
+			// Add LinkAliases to existing arguments:
+			node["kernelCmdlineArgs"] = fmt.Sprintf("%s %s", linkAliasesFinal, node["kernelCmdlineArgs"])
+		} else {
+			// No other arguments than LinkAliases:
+			node["kernelCmdlineArgs"] = linkAliasesFinal
+		}
+	}
+	return nil
 }
