bc60630c6c6880c035599c25223df72b540c08df
Raphaël Rodrigues <raphael.rodrigues@rayshift.net>
2026-09-10 09:57:37 +0200

    refactor(config): default kaas product configuration
    Signed-off-by: Raphaël Rodrigues <raphael.rodrigues@rayshift.net>

--- internal/superphenix-api/pkg/config/config.go
+++ internal/superphenix-api/pkg/config/config.go
@@ -298,9 +298,12 @@ productsConfig:
   argoApp:
     kubernetes:
       repo:
-        repoURL: ""
-        targetRevision: ""
-      kubeVersions: []
+      	repoURL: "ghcr.io/super-phenix/charts"
+		chart: "sfs-kaas"
+		targetRevision: "0.6.1"
+      kubeVersions:
+		- version: "v1.36.3"
+        - version: "v1.35.5"
       azDomains: {}
     backup:
       repo:
